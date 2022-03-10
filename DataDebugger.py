a3da96eecd08a63d37d39b93218cbc712676a1cf
INoodleI <chocowaffles2000@gmail.com>
2022-03-09 16:04:18 -0800

    Added git ignore

--- DataDebugger.py
+++ DataDebugger.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-ZetCode Tkinter tutorial
-
-The example draws lines on the Canvas.
-
-Author: Jan Bodnar
-Website: www.zetcode.com
-"""
-
 from tkinter import *
 import sys
 
@@ -26,10 +17,9 @@ class Point:
 def main():
     n = len(sys.argv)
     if(n != 5):
-        print("Usage: <Points.txt> <Triangles.txt> <Circles.txt> <DebugSquares.txt>")
+        print("Usage: <Points.txt> <Triangles.txt> <DebugSquares.txt> <DebugIntersections.txt>")
         return
 
-    
     root = Tk()
     root.title("Data Visualization")
     root.geometry("1000x700")
@@ -67,7 +57,15 @@ def main():
             canvas.create_line(p1.x,p1.y,p2.x,p2.y, p3.x, p3.y, p1.x, p1.y)
             line = f.readline().split()
     
-
+    #DebugCircles/Intersections
+    with open(sys.argv[4]) as f:
+        line = f.readline().split()
+        while line:
+            #print(line)
+            lineAsInt = list(map(float, line))
+            #print(lineAsInt)
+            pointsList.append( Point(lineAsInt[0]*spacing + offset, lineAsInt[1]*spacing+ offset ) )
+            line = f.readline().split()
 
     root.mainloop()
 
